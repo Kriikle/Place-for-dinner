4ff3495c973f3a885603cb447e46ae38c9e877fd
Kriikle <kirill.spainish.load.net@gmail.com>
2023-08-30 13:05:11 +0300

    Adding to migration first user creation

--- migration.py
+++ migration.py
@@ -1,5 +1,21 @@
 from core.models import restaurant, user
-from config.connection import SessionLocal, engine
+from config.connection import SessionLocal, engine, get_db
+from core.models.user import User
+from core.schemas.user import UserCreate
+from v1.functions.auth import get_password_hash
+from v1.functions.crud import create_
 
-restaurant.Base.metadata.create_all(bind=engine)
+
+db = get_db().__next__()
 user.Base.metadata.create_all(bind=engine)
+restaurant.Base.metadata.create_all(bind=engine)
+new_row = UserCreate(
+    login='Admin',
+    password='',
+    email='log@aq.ru',
+    firstname=None,
+    lastname=None,
+    patronymic=None
+)
+new_row.password = get_password_hash(new_row.password)
+create_(User, new_row, db)
