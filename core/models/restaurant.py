fb7fff622e1a16a9fcadae9c3b8c903b0aff957b
Kriikle <kirill.spainish.load.net@gmail.com>
2023-08-30 11:22:39 +0300

    Adding lat and lot

--- core/models/restaurant.py
+++ core/models/restaurant.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Double
 from sqlalchemy.orm import relationship, validates
 
 from config.connection import Base
@@ -11,4 +11,6 @@ class Restaurant(Base):
     name = Column(String)
     img_path = Column(String, nullable=True)
     address = Column(String, nullable=True)
+    lat = Column(Double, nullable=True)
+    lot = Column(Double, nullable=True)
 
--- core/schemas/restaurant.py
+++ core/schemas/restaurant.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, PositiveFloat
 
 
 class RestaurantBase(BaseModel):
     name: constr(max_length=120)
     img_path: constr(max_length=120)
     address: constr(max_length=120)
+    lat: float
+    lot: float
 
     class Config:
         from_attributes = True
