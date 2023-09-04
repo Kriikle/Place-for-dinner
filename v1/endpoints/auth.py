86ef6d8514b89c96c4b847ab1d330f4338cc0d2e
Kriikle <kirill.spainish.load.net@gmail.com>
2023-09-04 16:14:39 +0300

    register in auth path

--- v1/endpoints/auth.py
+++ v1/endpoints/auth.py
@@ -6,10 +6,13 @@ from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
+from core.models.user import User
 from core.schemas.token import TokenBase
-from v1.functions.auth import authenticate_user, create_access_token
+from core.schemas.user import UserRead, UserCreate
+from v1.functions.auth import authenticate_user, create_access_token, get_password_hash, get_current_user
 
 from config.connection import get_db
+from v1.functions.crud import create_
 
 router = APIRouter()
 
@@ -28,3 +31,15 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user.login}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/register/", response_model=UserRead)
+def register(new_row: UserCreate, db: Session = Depends(get_db)):
+    new_row.password = get_password_hash(new_row.password)
+    existing = db.query(User).filter((User.login == new_row.login) | (User.email == new_row.email)).all()
+    if existing:
+        raise HTTPException(status_code=409, detail="Login or email taken")
+    return create_(User, new_row, db)
+
+
+
