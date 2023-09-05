c8d5229ab5d5545eb5c80fa4e747a97c28bf132c
Kriikle <kirill.spainish.load.net@gmail.com>
2023-09-05 12:44:42 +0300

    params form environment

--- config/params.py
+++ config/params.py
@@ -1,12 +1,13 @@
-DB_TYPE = 1  # 1:sqlite, 2:postgres
-DB_SQLITE_NAME = 'weat.db'
-DB_HOST = ''
-DB_USERNAME = ''
-DB_PASSWORD = ''
-DB_PORT = ''
-DB_NAME = 'Some name'
+import os
+DB_TYPE = 1 if os.getenv('DB_TYPE') is None else os.getenv('DB_TYPE')  # 1:sqlite, 2:postgres
+DB_SQLITE_NAME = 'weat.db' if os.getenv('DB_SQLITE_NAME') is None else os.getenv('DB_SQLITE_NAME')
+DB_HOST = '' if os.getenv('DB_HOST') is None else os.getenv('DB_HOST')
+DB_USERNAME = '' if os.getenv('DB_USERNAME') is None else os.getenv('DB_USERNAME')
+DB_PASSWORD = '' if os.getenv('DB_PASSWORD') is None else os.getenv('DB_PASSWORD')
+DB_PORT = '' if os.getenv('DB_PORT') is None else os.getenv('DB_PORT')
+DB_NAME = 'Some name' if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
 TOKEN = {
-    'SECRET_KEY': '',
-    'ALGORITHM': '',
-    'ACCESS_TOKEN_EXPIRE_MINUTES': 30
+    'SECRET_KEY': "" if os.getenv('TOKEN_SECRET_KEY') is None else os.getenv('TOKEN_SECRET_KEY'),
+    'ALGORITHM': "" if os.getenv('TOKEN_ALGORITHM') is None else os.getenv('TOKEN_ALGORITHM'),
+    'ACCESS_TOKEN_EXPIRE_MINUTES': 180
 }
