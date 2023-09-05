d3a0649d9f66ea930cf6fbb492c7567d499e7e1f
Kriikle <kirill.spainish.load.net@gmail.com>
2023-09-05 12:56:27 +0300

    Chane to docker.yml

--- config/connection.py
+++ config/connection.py
@@ -8,18 +8,18 @@ path = os.getcwd()
 if DB_TYPE == 1:
     os_type = os.name
     if os_type == 'nt':
-        SQLALCHEMY_DATABASE_URL = "sqlite:///" + path + '\\' + DB_SQLITE_NAME
+        DATABASE_URL = "sqlite:///" + path + '\\' + DB_SQLITE_NAME
     else:
-        SQLALCHEMY_DATABASE_URL = "sqlite:///" + path + '/' + DB_SQLITE_NAME
+        DATABASE_URL = "sqlite:///" + path + '/' + DB_SQLITE_NAME
 elif DB_TYPE == 2:
-    SQLALCHEMY_DATABASE_URL = "postgresql://"\
+    DATABASE_URL = "postgresql://"\
                               + DB_USERNAME +\
                               ":" + DB_PASSWORD +\
                               "@" + DB_HOST +\
                               ':' + DB_PORT +\
                               "/" + DB_NAME
 elif DB_TYPE == 3:
-    SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://" \
+    DATABASE_URL = "mysql+mysqlconnector://" \
                               + DB_USERNAME +\
                               ":" + DB_PASSWORD +\
                               "@" + DB_HOST +\
@@ -27,7 +27,7 @@ elif DB_TYPE == 3:
                               "]/" + DB_NAME
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
