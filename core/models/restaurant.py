9c21ee9127e05c9a2ec67007e22e711c18a54d2e
Kriikle <kirill.spainish.load.net@gmail.com>
2023-11-02 10:44:26 +0300

    Cafe name not null

--- core/models/restaurant.py
+++ core/models/restaurant.py
@@ -10,7 +10,7 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id", ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
-    name = Column(String)
+    name = Column(String, nullable=False)
     is_public = Column(Boolean, nullable=False)
     img_path = Column(String, nullable=True)
     address = Column(String, nullable=True)
